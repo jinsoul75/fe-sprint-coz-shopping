53b7cae83240a3ea81af3446d6911ece617d1cb0
JINSOUL KIM <wlsthf75@gamil.com>
2023-05-17 10:58:31 +0900

    feature/footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,10 +2,10 @@ import classes from "./Footer.module.css";
 
 function Footer() {
     return(
-        <div className={classes.footer}>
+        <footer className={classes.footer}>
             <div>개인정보 처리방침 | 이용 약관</div>
             <div>All rights reserved @ Codestates</div>
-        </div>
+        </footer>
     )
 }
 
