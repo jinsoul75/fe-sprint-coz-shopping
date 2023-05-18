799444329e3563e32afcba49abc9e5e46c5d60f0
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-05-18 19:35:57 +0900

    edit ProductList

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useInView } from 'react-intersection-observer';
+import { useInView } from "react-intersection-observer";
 import Item from "../components/item/Item";
 import axios from "axios";
 import classes from "./ProductList.module.css";
@@ -16,17 +16,17 @@ function ProductList() {
   const EXHIBITION = "Exhibition";
   const BRAND = "Brand";
 
-  const fetchData = async () =>{
+  const fetchData = async () => {
     try {
       const res = await axios.get(
         "http://cozshopping.codestates-seb.link/api/v1/products"
       );
       setData(res.data);
-    } catch (error){
+    } catch (error) {
       setError("데이터를 가져오는 도중에 에러가 발생했습니다.");
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     fetchData();
@@ -77,29 +77,6 @@ function ProductList() {
           <span className={selectedType === BRAND && classes.clickedBtn}>
             브랜드
           </span>
-  
-  return (
-    <div className={classes.productListContainer}>
-      <div className={classes.filterBtn}>
-        <div onClick={()=>setSelectedType(null)}>
-          <img src='../images/filterBtnMain.svg' alt='main'></img>
-          <span className={selectedType === null&& classes.clickedBtn}>전체</span>
-        </div>
-        <div onClick={()=>setSelectedType('Product')}>
-          <img src='../images/filterBtnProduct.svg' alt='main'></img>
-          <span className={selectedType === 'Product'&& classes.clickedBtn}>상품</span>
-        </div>
-        <div onClick={()=>setSelectedType('Category')}>
-          <img src='../images/filterBtnCategory.svg' alt='main'></img>
-          <span className={selectedType === 'Category'&& classes.clickedBtn}>카테고리</span>
-        </div>
-        <div onClick={()=>setSelectedType('Exhibition')}>
-          <img src='../images/filterBtnExhib.svg' alt='main'></img>
-          <span className={selectedType === 'Exhibition'&& classes.clickedBtn}>기획전</span>
-        </div>
-        <div onClick={()=>setSelectedType('Brand')}>
-          <img src='../images/filterBtnBrand.svg' alt='main'></img>
-          <span className={selectedType === 'Brand'&& classes.clickedBtn}>브랜드</span>
         </div>
       </div>
       <ul className={classes.itemList}>
@@ -109,7 +86,7 @@ function ProductList() {
           data
             .filter((d) => selectedType === null || d.type === selectedType)
             .slice(visible.start, visible.end)
-            .map((d, idx) => <Item data={d} key={`id${idx}`} />)  
+            .map((d, idx) => <Item data={d} key={`id${idx}`} />)
         )}
         <div ref={ref}></div>
       </ul>
