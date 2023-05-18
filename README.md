2953b143abab6fd7b7c4e739890d9f11fdb38980
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-05-18 14:54:23 +0900

    문자열상수화

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
+<<<<<<< Updated upstream
 # fe-sprint-coz-shopping
+=======
+## 코드스테이츠 솔로프로젝트 쇼핑몰 북마크 구현
+<p align="center">
+  <img src="https://github.com/jinsoul75/fe-sprint-coz-shopping/assets/80370226/b6e40430-74c1-40ea-b7d7-8e10c4ec07f4">
+</p>
+
+## 사용스택
+- React + Hooks
+- Redux + Redux toolkit
+- React Icons
+- React-toastify
+- React-intersection-observer
+- React-router-dom
+- axios
+
+## 구현 내역
+- 라우팅
+- 무한스크롤
+- 로컬스토리지
+- Redux toolkit 활용 상태 전역 관리
+
+## 파일구조
+```
+📦src
+ ┣ 📂components
+ ┃ ┣ 📂footer
+ ┃ ┃ ┣ 📜Footer.js
+ ┃ ┃ ┗ 📜Footer.module.css
+ ┃ ┣ 📂header
+ ┃ ┃ ┣ 📜Header.js
+ ┃ ┃ ┗ 📜Header.module.css
+ ┃ ┣ 📂item
+ ┃ ┃ ┣ 📜Item.js
+ ┃ ┃ ┗ 📜Item.module.css
+ ┃ ┣ 📂list
+ ┃ ┃ ┣ 📜BookmarkList.js
+ ┃ ┃ ┣ 📜BookmarkList.module.css
+ ┃ ┃ ┣ 📜ProductList.js
+ ┃ ┃ ┗ 📜ProductList.module.css
+ ┃ ┗ 📂modal
+ ┃ ┃ ┣ 📜Modal.js
+ ┃ ┃ ┗ 📜Modal.module.css
+ ┣ 📂pages
+ ┃ ┣ 📜BookmarkList.js
+ ┃ ┣ 📜BookmarkList.module.css
+ ┃ ┣ 📜Main.js
+ ┃ ┣ 📜Main.module.css
+ ┃ ┣ 📜ProductList.js
+ ┃ ┗ 📜ProductList.module.css
+ ┣ 📂store
+ ┃ ┣ 📜bookmark.js
+ ┃ ┣ 📜index.js
+ ┃ ┗ 📜modal.js
+ ┣ 📜App.js
+ ┣ 📜App.module.css
+ ┣ 📜index.css
+ ┗ 📜index.js
+```
+>>>>>>> Stashed changes
---
+++ src/pages/ProductList.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import Item from "../components/item/Item";
+import axios from "axios";
+import classes from "./ProductList.module.css";
+
+function ProductList() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [ref, inView] = useInView();
+  const [selectedType, setSelectedType] = useState(null);
+  const [visible, setVisible] = useState({ start: 0, end: 16 });
+
+  const PRODUCT = "Product";
+  const CATEGORY = "Category";
+  const EXHIBITION = "Exhibition";
+  const BRAND = "Brand";
+
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(
+        "http://cozshopping.codestates-seb.link/api/v1/products"
+      );
+      setData(res.data);
+    } catch (error) {
+      setError("데이터를 가져오는 도중에 에러가 발생했습니다.");
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (inView) {
+      setVisible((prevData) => ({
+        start: prevData.start,
+        end: prevData.end + 16,
+      }));
+    }
+  }, [inView]);
+
+  useEffect(() => {
+    setVisible({ start: 0, end: 16 });
+  }, [selectedType]);
+
+  return (
+    <div className={classes.productListContainer}>
+      <div className={classes.filterBtn}>
+        <div onClick={() => setSelectedType(null)}>
+          <img src='../images/filterBtnMain.svg' alt='main'></img>
+          <span className={selectedType === null && classes.clickedBtn}>
+            전체
+          </span>
+        </div>
+        <div onClick={() => setSelectedType(PRODUCT)}>
+          <img src='../images/filterBtnProduct.svg' alt='main'></img>
+          <span className={selectedType === PRODUCT && classes.clickedBtn}>
+            상품
+          </span>
+        </div>
+        <div onClick={() => setSelectedType(CATEGORY)}>
+          <img src='../images/filterBtnCategory.svg' alt='main'></img>
+          <span className={selectedType === CATEGORY && classes.clickedBtn}>
+            카테고리
+          </span>
+        </div>
+        <div onClick={() => setSelectedType(EXHIBITION)}>
+          <img src='../images/filterBtnExhib.svg' alt='main'></img>
+          <span className={selectedType === EXHIBITION && classes.clickedBtn}>
+            기획전
+          </span>
+        </div>
+        <div onClick={() => setSelectedType(BRAND)}>
+          <img src='../images/filterBtnBrand.svg' alt='main'></img>
+          <span className={selectedType === BRAND && classes.clickedBtn}>
+            브랜드
+          </span>
+        </div>
+      </div>
+      <ul className={classes.itemList}>
+        {error ? (
+          <div>{error}</div> // 에러 메시지 출력
+        ) : (
+          data
+            .filter((d) => selectedType === null || d.type === selectedType)
+            .slice(visible.start, visible.end)
+            .map((d, idx) => <Item data={d} key={`id${idx}`} />)
+        )}
+        <div ref={ref}></div>
+      </ul>
+    </div>
+  );
+}
+
+export default ProductList;
